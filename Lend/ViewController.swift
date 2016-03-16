15c868a18075b26a2b5f8f3cb12230fbea125669
李文竣 <alexlee@alexleedemacbook-air.local>
2016-03-16 11:29:34 +0800

    Initial Commit

---
+++ Lend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lend
+//
+//  Created by alexlee on 16/3/16.
+//  Copyright © 2016年 bird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
